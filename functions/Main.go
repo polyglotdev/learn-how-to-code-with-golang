5901c8dc7f940ea100729ea78727fcba28c11db3
Dom Hallan <dom@polyglotdev.com>
2020-11-27 10:28:01 -0600

    🚀 complete the variadic functions section

--- functions/Main.go
+++ functions/Main.go
@@ -7,6 +7,10 @@ func main() {
 	bar("Ezra")
 	s1 := woo("Hallan")
 	fmt.Println(s1)
+	x, y := mouse("Elijah", "Hallan")
+	fmt.Println(x)
+	fmt.Println(y)
+	execute(2, 3, 4, 5, 6, 7, 8)
 }
 
 func foo() {
@@ -18,5 +22,20 @@ func bar(s string) {
 }
 
 func woo(st string) string {
-	return fmt.Sprint("Hello from woo,", st)
+	return fmt.Sprint("Hello from woo, ", st)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, " ", ln, `, says "Hello"`)
+	b := false
+
+	return a, b
+}
+
+func execute(x ...int) {
+	sum := 0
+	for _, v := range x {
+		sum += v
+		fmt.Println(sum)
+	}
 }
